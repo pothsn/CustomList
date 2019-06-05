7502484841b800a0a4282422b4b686ab265990c0
Nate Poths <pothsn@gmail.com>
2019-06-05 11:07:37 -0500

    Created test project/class and copied starter test methods.

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class CustomList
+    {
+        //Member variables (has a)
+
+
+
+        //Constructor (is a)
+
+
+
+        //Member methods (can do)
+        public void Add()
+        {
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
